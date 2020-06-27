b061f23d65fa99ccdfe3c84fc1c3624aa5affec3
Max Barrass <max.barrass@gmail.com>
2020-06-28 01:28:31 +1000

    update install after test

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 
 # enable non-free repos
+if [[ $(cat /etc/apt/sources.list.d/nonfree.list 2>/dev/null | grep "non-free rpi firmware") == "" ]]; then
 cat << EOF | sudo tee /etc/apt/sources.list.d/nonfree.list
 deb http://mirrordirector.raspbian.org/raspbian/ stretch main contrib non-free rpi firmware
 EOF
+fi
 
 #install packages
 sudo apt-get update && \
@@ -21,7 +23,6 @@ sudo apt-get install --no-install-recommends -y \
     python3-evdev \
     inotify-tools \
     ffmpeg \
-    ffmpeg-python \
     python-pyaudio \
     python-pydub \
     omxplayer \
@@ -30,13 +31,12 @@ sudo apt-get install --no-install-recommends -y \
     speech-dispatcher-espeak \
     alsa-base \
     alsa-tools \
-    alsa-utils \
-    omxplayer-wrapper \
-    jackd2
+    alsa-utils
 
-#set audio to use headphone jack, 2=hdmi
-amixer -c 1
+python3 -m pip install omxplayer-wrapper ffmpeg-python
 
+#set headphone jack volume to 100%
+amixer -c 1 sset Headphone 100%
 
 #configure alsa
 #update /usr/share/alsa/alsa.conf  with
@@ -47,28 +47,36 @@ amixer -c 1
 #OR
 cat config/.asoundrc > ~/.asoundrc
 cat config/.alsa > /usr/share/alsa/alsa.conf
-echo "export PA_ALSA_PLUGHW=1">/etc/environment
+if [[ $(cat /etc/environment 2>/dev/null | grep "export PA_ALSA_PLUGHW=1") == "" ]]; then
+sudo bash -c 'echo "export PA_ALSA_PLUGHW=1">/etc/environment'
+fi
 
 #disable all shortcuts
 cp config/rc.xml ~/.config/openbox
 
-
-#enable auto login
+#enable auto login, only once
+if [[ $(cat /etc/lightdm/lightdm.conf 2>/dev/null | grep "autologin-user=pi") == "" ]]; then
 cat << EOF | sudo tee /etc/lightdm/lightdm.conf
 [SeatDefaults]
 autologin-user=pi
 # Prevent the screen from shutting off automatically.
 xserver-command=X -s 0 dpms
 EOF
+fi
 
 
-
-#setup app
-
-mkdir -p $HOME/.config/openbox
-
+#setup app, only once
+if [[ ! -d $HOME/.config/openbox ]]; then
+    mkdir -p $HOME/.config/openbox
+fi
+if [[ $(cat $HOME/.config/openbox/autostart 2>/dev/null | grep "python3 -u $HOME/app.py") == "" ]]; then
 cat << EOF > $HOME/.config/openbox/autostart
 # redirect all output to a log file
 # -u so that output is flushed immediately to the log
 python3 -u $HOME/app.py > $HOME/app.log 2>&1 &
 EOF
+fi
+
+#test
+espeak "hello" --stdout | aplay --device=hw:1,0
+omxplayer /usr/share/sounds/speech-dispatcher/dummy-message.wav
